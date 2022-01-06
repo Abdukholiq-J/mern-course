a1d145ddb1471a38fb66497301c1370774f942d2
A <examle@gmail.com>
2022-01-06 22:15:09 +0500

    first commit

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const config = require('config')
 const mongoose = require('mongoose')
-const { path } = require('express/lib/application')
+const path = require('path')
 
 const app = express()
 
@@ -12,7 +12,7 @@ app.use('/api/link', require('./routes/link.routes'))
 app.use('/t', require('./routes/redirect.routes'))
 
 if (process.env.NODE_ENV === 'production') {
-    app.use('./', express.static(path.join(__dirname, 'client', 'build')))
+    app.use('/', express.static(path.join(__dirname, 'client', 'build')))
 
     app.get('*', (req, res) => {
         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
